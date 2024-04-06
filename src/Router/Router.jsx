45c3daea30d7475388e4018f2eb04a6240b8fb8f
adnanalemran <adnanalemranontor@gmail.com>
2024-04-06 12:25:39 +0600

    set up firebase

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
     {
-      path: "/signIn",
-      element: "login",
-    },     
+        path: "/",
+        element: "login",
+    },
+    {
+        path: "/signIn",
+        element: "login",
+    },
 ]);
 
 export default router;
