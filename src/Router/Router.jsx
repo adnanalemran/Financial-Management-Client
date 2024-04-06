b2977e99af667aec7e7dcbb60cb1e938fd68e48e
adnanalemran <adnanalemranontor@gmail.com>
2024-04-06 12:52:15 +0600

    sign in and sign up page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import Signin from "../Pages/Auth/Signin";
+import SignUp from "../Pages/Auth/SignUp";
 const router = createBrowserRouter([
     {
         path: "/",
         element: "login",
     },
+
+
     {
         path: "/signIn",
-        element: "login",
+        element: <Signin/>,
+    },  {
+        path: "/signUp",
+        element: <SignUp/>,
     },
 ]);
 
